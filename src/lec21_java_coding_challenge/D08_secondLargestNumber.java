c7bf8e2854608c89bde337b765432d371e8093d0
TofaelKabir <tofaelkabir@gmail.com>
2023-05-31 21:59:20 -0400

    Java Coding challenge 2

---
+++ src/lec21_java_coding_challenge/D08_secondLargestNumber.java
@@ -0,0 +1,21 @@
+package lec21_java_coding_challenge;
+
+import java.util.Arrays;
+
+//Que: To Raihan Khan, Can you find the second largest number from the Array
+
+public class D08_secondLargestNumber {
+	public static void main(String[] args) {
+		// This is not accepted by interviewer ( so try to avoid it)
+		int [] number = { 12, 10, 100, 5, 6, 2, 892, 77, 55, 7, 8, 5, 56, 323, 2, 44, 12, 1456, 6, 4, 5, 5 };
+		// Arrays class is used to manipulate the Array
+		System.out.println("Unsorted Array ::> " + Arrays.toString(number)); // no need
+		// sort method of Arrays class, is used to see the index of the Array in ascending order
+		Arrays.sort(number);
+		System.out.println("Sorted Array ::> " + Arrays.toString(number));
+		System.out.println("Smallest element is ::> " + number[0]);
+		System.out.println("Largest element is ::> " + number[number.length-1]);
+		System.out.println("Second Largest element is ::> " + number[number.length-2]);
+	}
+
+}
---
+++ src/lec21_java_coding_challenge/D09_SecondLargestNumber.java
@@ -0,0 +1,34 @@
+package lec21_java_coding_challenge;
+
+import java.util.Arrays;
+
+public class D09_SecondLargestNumber {
+
+	public static void main(String[] args) {
+
+		int[] number = { 12, 10, 100, 5, 6, 2, 892, 77, 55, 7, 8, 5, 56, 323, 44, 12, 1456, 6, 4, 5, 5, -1};
+		System.out.println(Arrays.toString(number));
+		// System.out.println(number.length);
+		
+		int temp;
+		
+		for (int i = 0; i < number.length; i++) {
+			for (int j = i+1; j<number.length; j++) {
+				if (number [i] > number [j]) {
+					temp = number [i];
+					number [i] = number[j];
+					number [j] = temp;
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(number));
+		System.out.println(number.length);
+		System.out.println("Second largest number is:: " + number[number.length - 2]);
+		System.out.println("Largest number is:: " + number[number.length - 1]);
+		System.out.println("Smallest number is:: " + number[0]);
+		System.out.println("Second Smallest number is:: " + number[1]);
+
+	}
+
+}
